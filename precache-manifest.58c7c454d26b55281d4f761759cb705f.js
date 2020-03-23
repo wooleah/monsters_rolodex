1a23c9a75513909e2a9f00adb4c9fe1bce78cbc7
wooleah <woojaeizzle@gmail.com>
2020-03-23 18:35:31 +0900

    Updates

---
+++ precache-manifest.58c7c454d26b55281d4f761759cb705f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7c9cd570ebc4d5dd1b47a997b3a973",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "c08b08426190a07a1fa2",
+    "url": "/monsters_rolodex/static/css/main.091521cb.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters_rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters_rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c08b08426190a07a1fa2",
+    "url": "/monsters_rolodex/static/js/main.0b0baf4e.chunk.js"
+  },
+  {
+    "revision": "c45966b3fc49e39bb793",
+    "url": "/monsters_rolodex/static/js/runtime-main.b97009c0.js"
+  }
+]);
